b1d3b8ea43f94c8268f448b8f43dd6901e5dba33
Abubakar Abid <a12d@stanford.edu>
2020-07-21 13:46:57 -0500

    fixed resizing issue

--- run-gradio.py
+++ run-gradio.py
@@ -14,11 +14,12 @@ inception_net = tf.keras.applications.InceptionV3()
 
 
 def classify_image_with_mobile_net(im):
-    arr = im.reshape((-1, 224, 224, 3))
+    im = Image.fromarray(im.astype('uint8'), 'RGB')
+    im = im.resize((224, 224))
+    arr = np.array(im).reshape((-1, 224, 224, 3))
     arr = tf.keras.applications.mobilenet.preprocess_input(arr)
     prediction = mobile_net.predict(arr).flatten()
     return {labels[i]: float(prediction[i]) for i in range(1000)}
-
 
 
 def classify_image_with_inception_net(im):
@@ -38,6 +39,7 @@ sample_images = [
                  ["sailboat.jpg"],
                  ["bicycle.jpg"]
 ]
+
 gr.Interface(
     [classify_image_with_mobile_net, classify_image_with_inception_net],
     imagein,
